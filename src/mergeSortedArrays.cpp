423720fd13148d9d75ae4b9ca49e9add5085d73d
vikramgaddam <vikramgaddam@missionrnd.com>
2016-03-17 01:29:45 +0530

    Submit Commit

--- src/mergeSortedArrays.cpp
+++ src/mergeSortedArrays.cpp
@@ -23,5 +23,109 @@ struct transaction {
 };
 
 struct transaction * mergeSortedArrays(struct transaction *A, int ALen, struct transaction *B, int BLen) {
-	return NULL;
+	
+	if ((A == NULL) || (B == NULL))return NULL;
+
+	struct transaction *res = (struct transaction *)malloc(sizeof(struct transaction)*(ALen + BLen));
+
+	int i = 0, j = 0, res_index = 0;
+
+	int input1_date, input1_month, input1_year, input2_date, input2_month, input2_year;
+
+	while ((i < ALen) && (j < BLen))
+	{
+		input1_date = A[i].date[1] - '0' + (A[i].date[0] - '0') * 10;
+		input1_month = A[i].date[4] - '0' + (A[i].date[3] - '0') * 10;
+		input1_year = (A[i].date[6] - '0') * 1000 + (A[i].date[7] - '0') * 100 + (A[i].date[8] - '0') * 10 + (A[i].date[9] - '0');
+
+		input2_date = B[j].date[1] - '0' + (B[j].date[0] - '0') * 10;
+		input2_month = B[j].date[4] - '0' + (B[j].date[3] - '0') * 10;
+		input2_year = (B[j].date[6] - '0') * 1000 + (B[j].date[7] - '0') * 100 + (B[j].date[8] - '0') * 10 + (B[j].date[9] - '0');
+
+
+
+		if (input1_year < input2_year)
+		{
+			res[res_index] = A[i];
+			i++;
+			res_index++;
+		}
+		else
+		{
+			if (input2_year < input1_year)
+			{
+				res[res_index] = B[j];
+				j++;
+				res_index++;
+			}
+			else
+			{
+				if (input1_month < input2_month)
+				{
+					res[res_index] = A[i];
+					i++;
+					res_index++;
+
+				}
+				else
+				{
+					if (input2_month < input1_month)
+					{
+						res[res_index] = B[j];
+						j++;
+						res_index++;
+					}
+					else
+					{
+						if (input1_date < input2_date)
+						{
+							res[res_index] = A[i];
+							i++;
+							res_index++;
+						}
+						else
+						{
+							if (input2_date < input1_date)
+							{
+								res[res_index] = B[j];
+								j++;
+								res_index++;
+							}
+							else
+							{
+								res[res_index] = A[i];
+								res_index++;
+								res[res_index] = B[j];
+								i++;
+								j++;
+								res_index++;
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	if (i <= (ALen-1))
+	{
+		while (i <= (ALen - 1))
+		{
+			res[res_index] = A[i];
+			i++;
+			res_index++;
+		}
+	}
+
+	if (j <= BLen - 1)
+	{
+		while (j <= BLen - 1)
+		{
+			res[res_index] = B[j];
+			j++;
+			res_index++;
+		}
+	}
+	
+	return res;
 }
